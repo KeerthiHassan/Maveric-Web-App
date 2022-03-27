e69e284dcf6127ec598b7377be0dd1fe0cf129a0
deepaksi7041 <deepaksi@maveric-systems.com>
2022-03-27 13:00:35 +0530

    Home and register pages are added

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer';
+import Posts from '../../components/Posts';
+import Header from '../../components/Header';
+
+
+const HomePage= ()=> {
+    return(
+        <div>
+        
+        <Header />
+         <Posts />
+        <Footer />
+        </div>
+        
+    )
+}
+export default HomePage;
